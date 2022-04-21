43cec985d1021eacfd98bed40c48831c0bdb6c99
yujindonut <hyujin0825@gmail.com>
2022-04-21 23:50:00 +0900

    add - tabbar delegation

--- Instagram-Clone/Instagram-Clone/Tabbar/TabBarViewController.swift
+++ Instagram-Clone/Instagram-Clone/Tabbar/TabBarViewController.swift
@@ -8,22 +8,34 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setDelegate()
+        setTabBarColor()
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setTabBarColor() {
+        tabBar.unselectedItemTintColor = .white
     }
-    */
-
+    private func setDelegate() {
+        self.delegate = self
+    }
+}
+extension TabBarViewController : UITabBarControllerDelegate {
+    //탭버튼이 클릭될때 호출됨
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        if selectedIndex == 2 {
+            tabBar.backgroundColor = .black
+            tabBar.barTintColor = .white
+            tabBar.tintColor = .white
+            tabBar.unselectedItemTintColor = .white
+        } else {
+            tabBar.backgroundColor = .white
+            tabBar.barTintColor = .black
+            tabBar.tintColor = .black
+            tabBar.unselectedItemTintColor = .black
+        }
+    }
 }
